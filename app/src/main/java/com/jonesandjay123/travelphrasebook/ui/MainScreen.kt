06ef779488b123ca205b867f2ca814f035d27ce8
joneswang <jonescwang@gmail.com>
2024-10-04 20:45:36 -0400

    remove version number

--- app/src/main/java/com/jonesandjay123/travelphrasebook/ui/MainScreen.kt
+++ app/src/main/java/com/jonesandjay123/travelphrasebook/ui/MainScreen.kt
@@ -97,7 +97,7 @@ fun MainScreen(
         topBar = {
             // 頂部應用欄，包含語言選擇按鈕
             TopAppBar(
-                title = { Text(text = "旅行短語手冊v0.0.10.3") },
+                title = { Text(text = "旅行短語手冊") },
                 actions = {
                     // 新的上傳到手錶按鈕
                     IconButton(onClick = {
